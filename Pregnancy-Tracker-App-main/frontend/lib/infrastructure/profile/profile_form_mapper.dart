623a7df90c1b575ceb8181e3a5b3121f2713640b
nkubana0 <i.shema@alustudent.com>
2025-03-22 02:27:41 +0200

    Add url_launcher dependency and update ProfileForm model

--- Pregnancy-Tracker-App-main/frontend/lib/infrastructure/profile/profile_form_mapper.dart
+++ Pregnancy-Tracker-App-main/frontend/lib/infrastructure/profile/profile_form_mapper.dart
@@ -13,3 +13,34 @@ extension PFMapper on ProfileForm {
         socialMedia: socialMedia);
   }
 }
+
+class ProfileFormMapper {
+  static ProfileForm fromJson(Map<String, dynamic> json) {
+    return ProfileForm(
+      firstName: json['firstName'] as String?,
+      lastName: json['lastName'] as String?,
+      followers: (json['followers'] as List<dynamic>?)
+          ?.cast<String>(), // Convert to List<String>
+      following: (json['following'] as List<dynamic>?)
+          ?.cast<String>(), // Convert to List<String>
+      profilePicture: json['profilePicture'] as String?,
+      socialMedia: (json['socialMedia'] as List<dynamic>?)
+          ?.cast<String>(), // Convert to List<String>
+      bio: json['bio'] as String?,
+      userName: json['userName'] as String?,
+    );
+  }
+
+  static Map<String, dynamic> toJson(ProfileForm profileForm) {
+    return {
+      'firstName': profileForm.firstName,
+      'lastName': profileForm.lastName,
+      'followers': profileForm.followers,
+      'following': profileForm.following,
+      'profilePicture': profileForm.profilePicture,
+      'socialMedia': profileForm.socialMedia,
+      'bio': profileForm.bio,
+      'userName': profileForm.userName,
+    };
+  }
+}
