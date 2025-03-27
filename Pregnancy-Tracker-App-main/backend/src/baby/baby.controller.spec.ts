8f85e07f6873ee185dedffb8f353af1200516617
cyloic <l.cyusa@alustudent.com>
2025-03-27 13:51:54 -0700

    changes

--- Pregnancy-Tracker-App-main/backend/src/baby/baby.controller.spec.ts
+++ Pregnancy-Tracker-App-main/backend/src/baby/baby.controller.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BabyController } from './baby.controller';
 
 describe('BabyController', () => {
-  let controller: BabyController;
+  let babyController: BabyController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BabyController],
     }).compile();
 
-    controller = module.get<BabyController>(BabyController);
+    babyController = module.get<BabyController>(BabyController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(babyController).toBeDefined();
   });
 });
