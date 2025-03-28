2ad083675f756ebee409a1894d1587c981f6a235
NickMuhigi <n.muhigi@alustudent.com>
2025-03-28 17:13:34 +0200

    profile test

--- Pregnancy-Tracker-App-main/frontend/tests/unit/repo_test/profile_repo_test.dart
+++ Pregnancy-Tracker-App-main/frontend/tests/unit/repo_test/profile_repo_test.dart
@@ -7,84 +7,137 @@ import 'package:frontend/domain/profile/profile_form.dart';
 import 'package:frontend/infrastructure/profile/profile_repository.dart';
 import 'package:mockito/mockito.dart';
 
+// Mock the ProfileRepository
 class MockProfileRepository extends Mock implements ProfileRepository {}
 
 void main() {
-  late ProfileRepository profileRepository;
   late MockProfileRepository mockProfileRepository;
+  late ProfileRepository profileRepository;
 
   setUp(() {
     mockProfileRepository = MockProfileRepository();
     profileRepository = ProfileRepository(mockProfileRepository as ProfileApi);
   });
 
-  group('updateProfile', () {
-    test('should return ProfileDomain when repository call is successful', () async {
-      // Arrange
-      final profileId = '123';
-      final profileForm = ProfileForm( bio: '', firstName: '', followers: [], following: [], lastName: '', profilePicture: '', socialMedia: []);
-      final profileDomain = ProfileDomain(id: profileId, bio: '', comments: [], firstName: '', followers: [], following: [], lastName: '', posts: [], profilePicture: '', socialMedia: [], userName: '');
+  group('ProfileRepository', () {
+    // Test for successful profile update
+    group('updateProfile', () {
+      test('should return ProfileDomain when repository call is successful',
+          () async {
+        // Arrange
+        final profileId = '123';
+        final profileForm = ProfileForm(
+            bio: '',
+            firstName: '',
+            followers: [],
+            following: [],
+            lastName: '',
+            profilePicture: '',
+            socialMedia: []);
+        final profileDomain = ProfileDomain(
+            id: profileId,
+            bio: '',
+            comments: [],
+            firstName: '',
+            followers: [],
+            following: [],
+            lastName: '',
+            posts: [],
+            profilePicture: '',
+            socialMedia: [],
+            userName: '');
 
-      when(mockProfileRepository.updateProfile(profileForm: profileForm, profileId: profileId))
-          .thenAnswer((_) async => right(profileDomain));
+        when(mockProfileRepository.updateProfile(
+                profileForm: profileForm, profileId: profileId))
+            .thenAnswer((_) async => right(profileDomain));
 
-      // Act
-      final result = await profileRepository.updateProfile(profileForm: profileForm, profileId: profileId);
+        // Act
+        final result = await profileRepository.updateProfile(
+            profileForm: profileForm, profileId: profileId);
 
-      // Assert
-      expect(result, equals(right(profileDomain)));
-      verify(mockProfileRepository.updateProfile(profileForm: profileForm, profileId: profileId)).called(1);
+        // Assert
+        expect(result, equals(right(profileDomain)));
+        verify(mockProfileRepository.updateProfile(
+                profileForm: profileForm, profileId: profileId))
+            .called(1);
+      });
+
+      test(
+          'should return ProfileFailure.serverError when repository call fails',
+          () async {
+        // Arrange
+        final profileId = '123';
+        final profileForm = ProfileForm(
+            bio: '',
+            firstName: '',
+            followers: [],
+            following: [],
+            lastName: '',
+            profilePicture: '',
+            socialMedia: []);
+
+        when(mockProfileRepository.updateProfile(
+                profileForm: profileForm, profileId: profileId))
+            .thenAnswer((_) async => left(ProfileFailure.serverError()));
+
+        // Act
+        final result = await profileRepository.updateProfile(
+            profileForm: profileForm, profileId: profileId);
+
+        // Assert
+        expect(result, equals(left(ProfileFailure.serverError())));
+        verify(mockProfileRepository.updateProfile(
+                profileForm: profileForm, profileId: profileId))
+            .called(1);
+      });
     });
 
-    test('should return ProfileFailure.serverError when repository call fails', () async {
-      // Arrange
-      final profileId = '123';
-      final profileForm = ProfileForm(bio: '', firstName: '', followers: [], following: [], lastName: '', profilePicture: '', socialMedia: []);
+    // Test for successful profile fetch
+    group('getProfile', () {
+      test('should return ProfileDomain when repository call is successful',
+          () async {
+        // Arrange
+        final userId = '123';
+        final profileDomain = ProfileDomain(
+            id: '123',
+            bio: '',
+            comments: [],
+            firstName: '',
+            followers: [],
+            following: [],
+            lastName: '',
+            posts: [],
+            profilePicture: '',
+            socialMedia: [],
+            userName: '');
 
-      when(mockProfileRepository.updateProfile(profileForm: profileForm, profileId: profileId))
-          .thenAnswer((_) async => left(ProfileFailure.serverError()));
+        when(mockProfileRepository.getProfile(userId))
+            .thenAnswer((_) async => right(profileDomain));
 
-      // Act
-      final result = await profileRepository.updateProfile(profileForm: profileForm, profileId: profileId);
+        // Act
+        final result = await profileRepository.getProfile(userId);
 
-      // Assert
-      expect(result, equals(left(ProfileFailure.serverError())));
-      verify(mockProfileRepository.updateProfile(profileForm: profileForm, profileId: profileId)).called(1);
+        // Assert
+        expect(result, equals(right(profileDomain)));
+        verify(mockProfileRepository.getProfile(userId)).called(1);
+      });
+
+      test(
+          'should return ProfileFailure.serverError when repository call fails',
+          () async {
+        // Arrange
+        final userId = '123';
+
+        when(mockProfileRepository.getProfile(userId))
+            .thenAnswer((_) async => left(ProfileFailure.serverError()));
+
+        // Act
+        final result = await profileRepository.getProfile(userId);
+
+        // Assert
+        expect(result, equals(left(ProfileFailure.serverError())));
+        verify(mockProfileRepository.getProfile(userId)).called(1);
+      });
     });
   });
-
-  group('getProfile', () {
-    test('should return ProfileDomain when repository call is successful', () async {
-      // Arrange
-      final userId = '123';
-      final profileDomain = ProfileDomain(id: '123', bio: '', comments: [], firstName: '', followers: [], following: [], lastName: '', posts: [], profilePicture: '', socialMedia: [], userName: '');
-
-      when(mockProfileRepository.getProfile(userId))
-          .thenAnswer((_) async => right(profileDomain));
-
-      // Act
-      final result = await profileRepository.getProfile(userId);
-
-      // Assert
-      expect(result, equals(right(profileDomain)));
-      verify(mockProfileRepository.getProfile(userId)).called(1);
-    });
-
-    test('should return ProfileFailure.serverError when repository call fails', () async {
-      // Arrange
-      final userId = '123';
-
-      when(mockProfileRepository.getProfile(userId))
-          .thenAnswer((_) async => left(ProfileFailure.serverError()));
-
-      // Act
-      final result = await profileRepository.getProfile(userId);
-
-      // Assert
-      expect(result, equals(left(ProfileFailure.serverError())));
-      verify(mockProfileRepository.getProfile(userId)).called(1);
-    });
-  });
-
-  // Add more unit tests for other methods in the ProfileRepository class
 }
