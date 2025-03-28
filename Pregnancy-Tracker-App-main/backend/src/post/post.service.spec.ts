8f52defd2fb786946343bb8b22a0bd183a20ea0f
cyloic <l.cyusa@alustudent.com>
2025-03-28 07:43:42 -0700

    debug

--- Pregnancy-Tracker-App-main/backend/src/post/post.service.spec.ts
+++ Pregnancy-Tracker-App-main/backend/src/post/post.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PostService } from './post.service';
 
 describe('PostService', () => {
-  let postService: PostService;
+  let service: PostService;
 
   beforeEach(async () => {
-    const testingModule: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       providers: [PostService],
     }).compile();
 
-    postService = testingModule.get<PostService>(PostService);
+    service = module.get<PostService>(PostService);
   });
 
-  it('should be instantiated properly', () => {
-    expect(postService).toBeDefined();
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
