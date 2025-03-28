bd9a8085e09ecf0a794eb33dae0c4a51738d2077
NickMuhigi <n.muhigi@alustudent.com>
2025-03-28 15:48:38 +0200

    MyApp Fix

--- Pregnancy-Tracker-App-main/frontend/test/widget_test.dart
+++ Pregnancy-Tracker-App-main/frontend/test/widget_test.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:frontend/application/post/post_list/bloc/post_list_bloc.dart';
 import 'package:frontend/main.dart';
+import 'package:frontend/application/post/post_list/bloc/post_list_bloc.dart';
 import 'package:frontend/application/note/bloc/note_bloc.dart';
-import 'widget_test.mocks.dart';
 import 'package:frontend/application/profile/bloc/profile_bloc.dart';
 import 'package:frontend/application/appointment/bloc/appointment_bloc.dart';
 import 'package:frontend/application/comment/bloc/comment_bloc.dart';
+import 'widget_test.mocks.dart';
 
-// Ensure this file is generated
-// Generate mocks for all required bloc
-@GenerateMocks([NoteBloc, PostListBloc, AppointmentBloc, ProfileBloc, CommentBloc])
+@GenerateMocks(
+    [NoteBloc, PostListBloc, AppointmentBloc, ProfileBloc, CommentBloc])
 void main() {
   testWidgets('Verify initial app structure', (WidgetTester tester) async {
     print('Test is running!');
+
     final mockNoteBloc = MockNoteBloc();
     final mockPostlistBloc = MockPostListBloc();
     final mockProfileBloc = MockProfileBloc();
@@ -23,10 +23,10 @@ void main() {
     final mockCommentBloc = MockCommentBloc();
 
     await tester.pumpWidget(
-      MaterialApp(  // Ensure MaterialApp wraps only once
+      MaterialApp(
         home: MyApp(
           noteBloc: mockNoteBloc,
-          postBloc: mockPostlistBloc,
+          postListBloc: mockPostlistBloc,
           profileBloc: mockProfileBloc,
           appointmentBloc: mockAppointmentBloc,
           commentBloc: mockCommentBloc,
@@ -34,8 +34,8 @@ void main() {
       ),
     );
 
-    await tester.pumpAndSettle(); // Ensure all animations/loaders settle
+    await tester.pumpAndSettle();
 
-    expect(find.byType(Scaffold), findsAtLeastNWidgets(1)); // Ensure at least one Scaffold
+    expect(find.byType(Scaffold), findsAtLeastNWidgets(1));
   });
 }
